fed435c5e810c651db116f248ac88036f7ac3506
Matheus <109323270+MatheusRb3@users.noreply.github.com>
2022-07-17 13:11:52 -0300

    Contador de números pares dentro de um array.

---
+++ ConParNumsArrays.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Deus e fiel
+ */
+public class ConParNumsArrays {
+   public static void main(String[] args){
+    
+    int[] parnums = {2,4,6,8,10};
+    int cont;
+    int i = 0;
+    
+    for(cont = 0;cont <=10;cont++){
+    cont = parnums[i];
+    
+    System.out.println(cont);
+    
+    i++;
+}
+}
+}
