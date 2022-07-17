2fd7bb90be634f97e8d7a9fead7b47bc5a159ad9
Matheus <109323270+MatheusRb3@users.noreply.github.com>
2022-07-17 13:18:29 -0300

    Contador de números de um array.

---
+++ ContArrays.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Deus e fiel
+ */
+public class ContArrays {
+ public static void main(String[] args){
+     int[] nums = {4,5,6,7,8,9,60,5,60};
+     int cont;
+     int i = 0;
+     
+     for(i = 0;i <=8;i++){
+         cont = nums[i];
+         System.out.println(cont);
+     }
+}
+}
